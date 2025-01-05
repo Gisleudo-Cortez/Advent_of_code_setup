e4ecc823c8ae83efa9c8576eeda39133be3939df
Gisleudo <gisleudo.cmf@gmail.com>
2025-01-05 11:13:08 -0300

    Update folder creation to check if year already in current path

--- main.py
+++ main.py
@@ -3,23 +3,38 @@ import argparse
 import os
 
 def create_day_and_year_folder(year: str, day: str):
-    os.makedirs(f"{year}/{day}", exist_ok=True)
+    current_dir = os.getcwd()
+    if year in current_dir:
+        full_path = f"{day}"
+    else:
+        if not os.path.exists(year):
+            os.makedirs(year)
+        full_path = f"{year}/{day}"
+    
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+    return full_path
 
 def get_input_data(year: str, day: str, session: str):
     input_url = f"https://adventofcode.com/{year}/day/{day}/input"
     cookies = dict(session=session)
     response = requests.get(input_url, cookies=cookies)
-    
+
     if response.status_code == 200:
         input_file = response.text
-        file_path = f"{year}/{day}/input.txt"
+        base_path = day if year in os.getcwd() else f"{year}/{day}"
+        file_path = f"{base_path}/input.txt"
 
         if not os.path.isfile(file_path):
             with open(file_path, 'w') as file:
                 file.write("")
-
+                
         with open(file_path, 'w') as file:
             file.write(input_file)
+
+        # Create empty example.txt file
+        with open(f"{base_path}/example.txt", "w") as file:
+            file.write("")
     else:
         print(f"Failed to download input data. Status code: {response.status_code}")
 
@@ -28,8 +43,8 @@ def main():
     parser.add_argument("-d", "--day", required=True, type=str, help="Day of the challenge from 1 - 25")
     parser.add_argument("-y", "--year", required=True, type=str, help="Year of the challenge starting from 2015")
     parser.add_argument("-s", "--session", required=True, type=str, help="Your session cookie")
-    
     args = parser.parse_args()
+
     create_day_and_year_folder(args.year, args.day)
     get_input_data(args.year, args.day, args.session)
 
